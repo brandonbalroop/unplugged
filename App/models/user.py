887570261445455b7b87466e306e709ff5810d17
brandonbalroop <78388649+brandonbalroop@users.noreply.github.com>
2022-04-23 23:14:11 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -1,20 +1,18 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-from App.database import db
+from App.database import 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    routeslist = db.relationship('Routeslist', backref='user_routeslist')
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
-
-    def toDict(self):
-        return{
-            'id': self.id,
-            'username': self.username
-        }
+     def toDict(self):
+      return {
+        "id": self.id,
+        "username": self.username,
+        "password": self.password
+      }
 
     def set_password(self, password):
         """Create hashed password."""
@@ -23,4 +21,32 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+
+class Routeslist(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    route = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    startlocation = db.Column(db.String(120), nullable=False)
+    endlocation = db.Column(db.String(120), nullable=False)
+    lastupdate= db.Column(db.String(120), nullable=False)
+    distance= db.Column(db.Float, nullable=False)
+    maxifare= db.Column(db.Integer, nullable=False)
+    taxifare= db.Column(db.Integer, nullable=False)
+    routeslist = db.relationship('Routeslist', backref='route_routeslist')
+    
+
+    def toDict(self):
+        return {
+            "id":self.id,
+            "startlocation":self.startlocation,
+            "endlocation":self.endlocation,
+            "lastupdate":self.lastupdate,
+            "distance":self.distance,
+            "maxifare":self.maxifare,
+            "taxifare":self.taxifare
+        }
+
 
